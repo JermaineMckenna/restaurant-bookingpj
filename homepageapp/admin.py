48fd301ea87c9ec4b3757ffe461fe2e0954b6d4a
JermaineMckenna <jermaine.mckenna_@outlook.com>
2025-11-16 10:19:37 +0000

    tables and menu item for postgresql database

--- homepageapp/admin.py
+++ homepageapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Booking, ContactMessage
+from .models import Booking, ContactMessage, Table, MenuItem
 
+admin.site.register(Table)
+admin.site.register(MenuItem)
 admin.site.register(Booking)
 admin.site.register(ContactMessage)
