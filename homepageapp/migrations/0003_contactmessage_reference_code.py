85f4f6e6d18bb693259572d76916ab3fd516ed2a
JermaineMckenna <jermaine.mckenna_@outlook.com>
2025-12-16 12:24:23 +0000

    fixed reference code error

--- homepageapp/migrations/0003_contactmessage_reference_code.py
+++ homepageapp/migrations/0003_contactmessage_reference_code.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='contactmessage',
             name='reference_code',
-            field=models.CharField(default='TEMPMSG00000', editable=False, max_length=12, unique=True),
+            field=models.CharField(default='TEMPMSG00000', editable=False, max_length=20, unique=True),
             preserve_default=False,
         ),
     ]
